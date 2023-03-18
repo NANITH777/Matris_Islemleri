582d8455cf2b6519182aa6ace0a42be13173e5e5
NANITH777 <nanifulchany@gmail.com>
2023-03-18 12:23:23 +0300

    declared functions and adding libraries

---
+++ matris-islemleri/matris-islemleri.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<iomanip>         //setw() kullanabilmek icin
+#include<locale>         // Diller ve karakter setleri kütüphanesi
+#include<Windows.h>      // yazı rengi için
+
+using namespace std;
+
+float matrisa[10][10], matrisb[10][10], matrisc[10][10], transpomatris[10][10];
+float tersmatris[10][10];   // 10 sayisi maksimum olarak alinir
+unsigned int satir, sutun;
+int iz = 0;
+
+void matrisToplama();
+void matrisCikarma();
+void matrisCarpma();
+void matrisIzBulma();
+void matrisTranspozesi();
+void matrisTersi();
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
